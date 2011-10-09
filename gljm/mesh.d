a35832006ab5b44ee100d0b16e18107b68b09921
David Herberth <admin@dav1d.de>
2011-10-09 19:06:31 +0200

    added a mesh.d with content

---
+++ gljm/mesh.d
@@ -0,0 +1,29 @@
+module gljm.mesh;
+
+private {
+    import std.variant : Variant;
+    import gljm.vbo;
+}
+
+
+struct Mesh { // well it's just a container
+    private Variant[string] _members;
+    
+    Variant opDispatch(string s)() {
+        return get(s);
+    }
+    
+    void set(T)(string s, T v) {
+        if(Variant.allowed!T) {
+            _members[s] = v;
+        } else {
+            throw new Exception(T.stringof ~ " not allowed as Variant");
+        }
+    }
+    
+    Variant get(string s) {
+        return _members[s];
+    }
+    
+}
+
