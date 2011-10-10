5682285304a481a151146f955f664eec7cc3ee8a
David Herberth <admin@dav1d.de>
2011-10-10 19:44:06 +0200

    now you can also draw the meshes

--- gljm/mesh.d
+++ gljm/mesh.d
@@ -5,7 +5,9 @@ private {
     import libdjson.json : JSONType, JSONObject, readJSON;
     import std.array : split;
     import std.conv : to;
-    import derelict.opengl.gl : GLint, GL_FLOAT, GL_UNSIGNED_SHORT, GL_INT;
+    import derelict.opengl.gl : GLint, GLsizei, GLuint, GLenum,
+                                glDrawElements, glDrawArrays,
+                                GL_TRIANGLES, GL_FLOAT, GL_UNSIGNED_SHORT, GL_INT;
     import gljm.util : conv_array;
     import gljm.vbo : Buffer, ElementBuffer, BufferData;
 }
@@ -14,26 +16,73 @@ private {
 struct Mesh {
     struct ElementBufferS {
         private ElementBuffer[string] _members;
+        private int count = 0;
         alias _members this;
     
         ElementBuffer opDispatch(string s)() { return get(s); }
-        package void set(string s, ElementBuffer b) { _members[s] = b; }
         ElementBuffer get(string s) { return _members[s]; }
+        package void set(string s, ElementBuffer b) {
+            _members[s] = b;
+            
+            if(!count) {
+                count = b.buffer_data.data.length;
+            }
+        }
+        
+        void bind() { // kinda strange, more then one ELEMENT_ARRAY_BUFFER?
+            foreach(string key, ElementBuffer value; _members) {
+                value.bind();
+            }
+        }
     }
     
     struct BufferS {
         private Buffer[string] _members;
+        private int count = 0;
         alias _members this;
     
         Buffer opDispatch(string s)() { return get(s); }
-        package void set(string s, Buffer b) { _members[s] = b; }
         Buffer get(string s) { return _members[s]; }
+        package void set(string s, Buffer b) {
+            _members[s] = b;
+            
+            if(!count) {
+                count = b.buffer_data.data.length / b.buffer_data.size;
+            }
+        }
+        
+//         void bind() { // you dont want this!
+//             foreach(string key, Buffer value) {
+//                 value.bind(); 
+//             }
+//         }
+        
+        void bind(GLuint[string] attrib_locations) {
+            foreach(string key, Buffer value; _members) {
+                value.bind(attrib_locations[key]);
+            }
+        }
     }
     
     ElementBufferS element_buffer;
     BufferS buffer;
+    
+    @property int count() {
+        if(element_buffer) { return element_buffer.count; }
+        else { return buffer.count; }
+    }
+    
+    void draw(GLuint[string] attrib_locations, GLenum mode = GL_TRIANGLES, GLint offset = 0, GLsizei count_ = -1) {
+        buffer.bind(attrib_locations);
+        if(element_buffer) {
+            element_buffer.bind();
+            
+            glDrawElements(mode, (count_<0?element_buffer.count:count_), GL_UNSIGNED_SHORT, cast(void *)(offset));
+        } else {
+            glDrawArrays(mode, offset, (count_<0?buffer.count:count_));
+        }
+    }
 }
-
 
 Mesh load_mesh(JSONObject jobj) {
     Mesh m;
